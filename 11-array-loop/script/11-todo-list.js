f14c6832b347bf9ac4f3291e07bfbf2e9ca70641
Chaitanya <chaitanyayadwad8@gmail.com>
2025-06-23 20:52:39 +0530

    array n loop

---
+++ 11-array-loop/script/11-todo-list.js
@@ -0,0 +1,23 @@
+const todoList = [];
+renderTodoList();
+function renderTodoList() {
+  let todoListHtml = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html;
+  }
+  console.log(todoListHtml);
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHtml;
+}
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  console.log(name);
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+  renderTodoList();
+}
